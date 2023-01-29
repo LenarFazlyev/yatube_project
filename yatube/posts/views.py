7124de633368aa0f1505cf9a1b8412b61c824005
Lenar <lr.fazliev@gmail.com>
2023-01-29 22:17:47 +0300

    Load static

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,12 +1,27 @@
-#from django.shortcuts import render
+from django.shortcuts import render
 
 # Create your views here.
 
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Главная страница')
+    template = 'posts/index.html'
+    title = 'Это главная страница проекта Yatube'
+    context = {
+        'title': title,
+        'template': template,
+    }
+    return render(request, template, context)
+    #return HttpResponse('Главная страница')
 
 
 def group_posts(request, slug):
-    return HttpResponse(f"Посты отфильтрованные по группам {slug}")
+    template = 'posts/group_list.html'
+    title = 'Здесь будет информация о группах проекта Yatube'
+    context = {
+        'title': title,
+        'template': template,
+        'slug': slug,
+    }
+    return render(request, template, context)
+    #return HttpResponse(f"Посты отфильтрованные по группам {slug}")
